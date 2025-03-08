48bcc09bdc82f812253cb2138c469db19eea1600
Alfredo Rivera <aaro700930@gmail.com>
2025-03-07 20:04:32 -0800

    Builder utilizacion

--- 01-creacionales/01-builder.ts
+++ 01-creacionales/01-builder.ts
@@ -1,3 +1,4 @@
+import { COLORS } from '../helpers/colors.ts';
 /**
  * ! Patrón Builder:
  * Es un patrón de diseño creacional que nos permite construir objetos complejos
@@ -12,3 +13,81 @@
  *
  * https://refactoring.guru/es/design-patterns/builder
  */
+
+
+class Computer {
+    public cpu: string = 'cpu - not defined';
+    public ram: string = 'ram - not defined';
+    public storage: string = 'storage - not defined';
+    public gpu?: string;
+
+    displayConfiguration() {
+        console.log(`Configuracion de la computadora
+            CPU: ${this.cpu}
+            RAM: ${this.ram}
+            Almacenamiento: ${this.storage}
+            GPU: ${this.gpu ? this.gpu : 'No tiene GPU'}
+        `);
+    }
+
+}
+
+class ComputerBuilder {
+
+    private computer: Computer;
+
+    constructor() {
+        this.computer = new Computer();
+    }
+
+    setCPU(cpu: string): ComputerBuilder {
+        this.computer.cpu = cpu;
+        return this;
+    }
+
+    setRAM(ram: string): ComputerBuilder {
+        this.computer.ram = ram;
+        return this;
+    }
+
+    setStorage(storage: string): ComputerBuilder {
+        this.computer.storage = storage;
+        return this;
+    }
+
+    setGPU(gpu: string): ComputerBuilder {
+        this.computer.gpu = gpu;
+        return this;
+    }
+
+    build(): Computer {
+        return this.computer;
+    }
+
+}
+
+function main() {
+
+    const basicComputer = new ComputerBuilder()
+        .setCPU('i3')
+        .setRAM('4GB')
+        .setStorage('256GB HDD')
+        .build();
+
+
+
+    console.log('%cComputadora básica:', COLORS.blue);
+    basicComputer.displayConfiguration();
+
+    const gamerComputer = new ComputerBuilder()
+        .setCPU('i9')
+        .setRAM('128GB')
+        .setStorage('1TB SSD')
+        .setGPU('RTX 5090')
+        .build();
+
+    console.log('%cComputadora Gamer:', COLORS.blue);
+    gamerComputer.displayConfiguration();
+}
+
+main();
